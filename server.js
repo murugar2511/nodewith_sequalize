98cff560b5e96d5a87cfbe8cbea5ded9cac6f98e
mathavan <mathavan@twilightsoftwares.com>
2024-09-19 13:40:35 +0530

    #firstcommit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const app = express();
+
+var corOptions ={
+    origin :'http://localhost:8081'
+}
+
+app.use(cors(corOptions))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get('/',(req,res)=>{
+    res.json({ message :'hellow form api'})
+})
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT,()=>{
+    console.log(`server is rnng ${PORT}`)
+    
+})
