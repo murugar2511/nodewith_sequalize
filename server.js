580381de566f1512924d28b2e7bad7f5d9dee420
mathavan <mathavan@twilightsoftwares.com>
2024-09-19 13:55:01 +0530

    #added name

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.get('/',(req,res)=>{
-    res.json({ message :'hellow form api'})
+    res.json({ message :'hellow form api created by mathavan'})
 })
 
 const PORT = process.env.PORT || 8080
